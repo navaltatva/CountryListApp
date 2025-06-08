5e89139703144d2d7685aa6674c1867080c52744
Naval <navalhsn@gmail.com>
2025-06-08 13:59:06 +0530

    initial commit

---
+++ CountriesListApp/CountriesListAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  CountriesListAppApp.swift
+//  CountriesListApp
+//
+//  Created by Naval Hasan on 08/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CountriesListAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ CountriesListAppTests/CountriesListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesListAppTests.swift
+//  CountriesListAppTests
+//
+//  Created by Naval Hasan on 08/06/25.
+//
+
+import Testing
+@testable import CountriesListApp
+
+struct CountriesListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
